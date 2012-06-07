6bcb4c3e51bf927af6cc4aaa66f1838746a2e2b4
Alex Fu <alex.fu914@gmail.com>
2012-06-07 13:00:03 -0400

    Reorganized project directory structure.

--- src/com/gmail/at/faint545/activities/SabFragmentActivity.java
+++
@@ -1,7 +0,0 @@
-package com.gmail.at.faint545.activities;
-
-import com.actionbarsherlock.app.SherlockFragmentActivity;
-
-public abstract class SabFragmentActivity extends SherlockFragmentActivity {
-  public abstract void showErrorDialog(CharSequence message);
-}
--- src/com/gmail/at/faint545/interfaces/CheckChangedListener.java
+++
@@ -1,5 +0,0 @@
-package com.gmail.at.faint545.interfaces;
-
-public interface CheckChangedListener {
-  public void onCheckChanged(boolean isChecked, int checkCount);
-}
