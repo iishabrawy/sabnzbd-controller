e4a03183c75e2b18ae5edda7c10503067bded5d7
Alex Fu <alex.fu914@gmail.com>
2012-06-14 12:46:59 -0400

    Implemented textAllCaps for pre ICS

--- app/src/com/gmail/at/faint545/views/StyledTextView.java
+++ app/src/com/gmail/at/faint545/views/StyledTextView.java
@@ -3,34 +3,51 @@ package com.gmail.at.faint545.views;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Typeface;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.widget.TextView;
-
 import com.gmail.at.faint545.R.styleable;
+import com.gmail.at.faint545.utils.AllCapsTransformationMethod;
 
 public class StyledTextView extends TextView {
+
+  private final boolean isICS = Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH;
+
 	// For debugging
-	private static final String LOGTAG = StyledTextView.class.getSimpleName();
+	private static final String LOGTAG = "StyledTextView";
 	
 	public StyledTextView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
-		setCustomFontFace(context, attrs);
+		applyStyle(context, attrs);
 	}
 
 	public StyledTextView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		setCustomFontFace(context, attrs);
+		applyStyle(context, attrs);
 	}
 
 	public StyledTextView(Context context) {
 		super(context);
 	}
 
-	private void setCustomFontFace(Context context, AttributeSet attrs) {
+	private void applyStyle(Context context, AttributeSet attrs) {
 		if(!isInEditMode()) {
 			TypedArray attrArray = context.obtainStyledAttributes(attrs, styleable.StyledTextView);
-			String assetPath = attrArray.getString(styleable.StyledTextView_fontface);			
-			setTypeface(Typeface.createFromAsset(getContext().getAssets(), assetPath));
+
+      String fontFacePath = attrArray.getString(styleable.StyledTextView_fontface);
+			setTypeface(Typeface.createFromAsset(getContext().getAssets(), fontFacePath));
+
+      if(!isICS) {
+        boolean allCaps = attrArray.getBoolean(styleable.StyledTextView_textAllCaps,false);
+        setAllCapsCompat(allCaps);
+      }
 		}
 	}
+
+  private void setAllCapsCompat(boolean allCaps) {
+    if(allCaps)
+      setTransformationMethod(new AllCapsTransformationMethod(getContext()));
+    else
+      setTransformationMethod(null);
+  }
 }
